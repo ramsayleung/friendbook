98b7f15109ae555d96f8dcbab052278295455925
Ramsay Leung <ramsayleung@gmail.com>
2024-02-28 19:13:43 -0800

    Initial Commit

---
+++ FriendWise/FriendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendWiseApp.swift
+//  FriendWise
+//
+//  Created by ramsayleung on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct FriendWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
